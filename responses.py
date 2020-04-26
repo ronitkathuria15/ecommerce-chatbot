9e9599fac615f8eb21a7de5995014587e3bd95fa
Ronit Kathuria <ronitkats15@gmail.com>
2020-04-26 21:30:23 +0530

    Create responses.py

---
+++ responses.py
@@ -0,0 +1,10 @@
+PART_PRICE  = [
+    'Your part ({name}) costs $ {price}.' ,
+    'The price of "{name}" is $ {price}.' ,
+    'The product "{name}" has a cost of $ {price}.' ,
+]
+
+
+INFO  = [
+    'ID: {id} \ n Name: {name} \ n Description: {description} \ n Price: $ {price}.'
+]
